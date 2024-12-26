ef55f4147c9ce9fe0ad2340a2095f476ee1521db
sabalesameer <136320017+sabalesameer@users.noreply.github.com>
2024-12-26 19:46:39 +0530

    Update createCard.jsx

--- frontend/my-app/src/cpmponent/createCard.jsx
+++ frontend/my-app/src/cpmponent/createCard.jsx
@@ -25,7 +25,7 @@ const CreateCard = () => {
     setSuccess(null);
 
     try {
-      const response = await fetch("http://localhost:5000/api/cards/create", {
+      const response = await fetch("https://mern-wt.onrender.com/api/cards/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
