6da320cfc48038e27481dd960c17bdc6c1a0c511
sabalesameer <136320017+sabalesameer@users.noreply.github.com>
2024-12-26 19:49:35 +0530

    Update ragister.jsx

--- frontend/my-app/src/cpmponent/ragister.jsx
+++ frontend/my-app/src/cpmponent/ragister.jsx
@@ -28,7 +28,7 @@ const Register = () => {
     setSuccess(null);
 
     try {
-      const response = await fetch("http://localhost:5000/api/users/register", {
+      const response = await fetch("https://mern-wt.onrender.com/api/users/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
